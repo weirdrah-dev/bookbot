4cb6133567e279678c947de025bfd9d6c598a663
weirdrah-dev <weirdrah@gmail.com>
2024-11-19 17:49:35 -0700

    added report to main.py, moved print functions to report.

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(text)
+    #print(text)
     word_count = get_book_length(text)
-    print(f"{word_count} words found in document.")
-    print(get_character_count(text))
+    #print(f"{word_count} words found in document.")
+    chars = get_character_count(text)
+    report(book_path,word_count,chars)
 
 def get_book_text(path):
     with open(path) as f:
@@ -22,5 +23,12 @@ def get_character_count(text):
         else:
             character_count[i] = 1
     return character_count
+
+def report(book_path,word_count,chars):
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{word_count} words found in the document")
+    for i in chars:
+        print(f"The '{i}' character was found {chars[i]} times")
+    print("--- End report ---")
 
 main()
