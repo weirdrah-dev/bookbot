f4232fd84cbbb2d3e63f1acf62ffaabbed7185de
weirdrah-dev <weirdrah@gmail.com>
2024-11-19 17:42:05 -0700

    added get_character_count to main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ def main():
     print(text)
     word_count = get_book_length(text)
     print(f"{word_count} words found in document.")
+    print(get_character_count(text))
 
 def get_book_text(path):
     with open(path) as f:
@@ -12,5 +13,14 @@ def get_book_text(path):
 def get_book_length(text):
     book_words = text.split()
     return len(book_words)
+
+def get_character_count(text):
+    character_count = {}
+    for i in text.lower():
+        if i in character_count:
+            character_count[i] = character_count[i] + 1
+        else:
+            character_count[i] = 1
+    return character_count
 
 main()
