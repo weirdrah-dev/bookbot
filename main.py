544707a596e03a3b03ef215132146923100bdd12
weirdrah-dev <weirdrah@gmail.com>
2025-05-06 17:01:24 -0600

    refactored number of words method into stats

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+from stats import get_num_words
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     #print(text)
-    word_count = get_book_length(text)
+    word_count = get_num_words(text)
     #print(f"{word_count} words found in document.")
     chars = get_character_count(text)
     report(book_path,word_count,chars)
@@ -10,10 +11,6 @@ def main():
 def get_book_text(path):
     with open(path) as f:
         return f.read()
-
-def get_book_length(text):
-    book_words = text.split()
-    return len(book_words)
 
 def get_character_count(text):
     character_count = {}
---
+++ stats.py
@@ -0,0 +1,3 @@
+def get_num_words(text):
+    book_words = text.split()
+    return len(book_words)
